ebd0a3cd609fbaf2118091421962f7ebbafdb9f7
Ashirvad-Singh <ashirvad2912@gmail.com>
2024-01-24 17:12:25 +0530

    Day1 completed

--- week1/day1.java
+++ week1/day1.java
@@ -14,13 +14,24 @@ public class day1 {
         int B = sc.nextInt();
         int C = sc.nextInt();
 
-        // Calculate the average
+        // // Calculate the average
         float N = (A + B + C) / 3;
 
-        // Output the average
-        System.out.println("The average of the three numbers is: " + N);
+        // // Output the average
+         System.out.println("The average of the three numbers is: " + N);
 
-        // Inaprogram,inputthesideofasquare.
-        Youhavetooutputtheareaofthesquare.(Hint : area of a square is (side x side))
+        //Enter cost of 3 items from the user(usingfloatdatatype)
+        // -a pencil,a pen  and an eraser. You have to output the total 
+        // cost of the items back to the user as their bill.
+        // (Add on : You can also try adding 18% gst tax to the items 
+        // in the bill as an advanced problem   
+        System.out.println("Enter the thing value:");
+        float pen = sc.nextFloat();
+        float pencil = sc.nextFloat();
+        float eraser = sc.nextFloat();  
+        float total=pen+pencil+eraser;
+        System.out.println("Bill is:"+total);
+        float newTotal=total+(0.18f*total);
+        System.out.println("Cost After GST:"+newTotal);
     }
 }
